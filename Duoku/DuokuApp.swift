769533271fcf4289aac6b5ab6f33968c331ba858
Jan Lesák <jan.lesak@greencode.cz>
2025-02-11 10:15:01 +0100

    Initial Commit

---
+++ Duoku/DuokuApp.swift
@@ -0,0 +1,32 @@
+//
+//  DuokuApp.swift
+//  Duoku
+//
+//  Created by Jan Lesák on 11.02.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DuokuApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DuokuTests/DuokuTests.swift
@@ -0,0 +1,17 @@
+//
+//  DuokuTests.swift
+//  DuokuTests
+//
+//  Created by Jan Lesák on 11.02.2025.
+//
+
+import Testing
+@testable import Duoku
+
+struct DuokuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
